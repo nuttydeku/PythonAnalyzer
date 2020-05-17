651b75976618db944fe4d92c0a1037b12252920d
nuttydeku <rj.copeland@bellevuecollege.edu>
2020-05-17 01:24:52 -0700

    Update driver

---
+++ Driver.py
@@ -0,0 +1,37 @@
+import io
+
+if __name__ == '__main__':
+
+    def analyze():
+        pass
+
+    string = "This is a long string of crap that i want to not be so crappy; ThE IntEEENT is to tokenize and analyze"
+
+
+    word_tokens = []
+    for s in string:
+        print(word_tokenize(string))
+        word_tokens.append(word_tokenize(string))
+
+    # def extract_data(path):
+    #     try:
+    #         writetofile = open(os.path.join(path, "wigglefile"), "a")
+    #     except:
+    #         print("Write to file failed")
+    #         return
+    #     else:
+    #         for filename in os.listdir(path):
+    #             try:
+    #                 with open(os.path.join(path, filename), 'r') as f:
+    #                     writetofile.write(f.read())
+    #             except:
+    #                 print("File " + filename + " failed to open")
+    #                 print("Check " + os.path.join(path, filename) + " for errors")
+    #
+    #         f.close()
+    #         writetofile.close()
+    #
+    # extract_data("/home/rcoper/Desktop/PythonReadFolder/")
+
+
+
